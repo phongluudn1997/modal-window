a2e9b0bfb0b7bae7ad8e2366f843da1fe305cddd
Phong Luu <huynhpluu@gmail.com>
2020-10-25 17:10:09 +0700

    added: close modal on Esc

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.close-modal');
 const btnShow = document.querySelectorAll('.show-modal');
+const ESC_KEY_CODE = 27;
 
 function showModal() {
-  console.log('click');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 }
@@ -16,9 +16,15 @@ function closeModal() {
   overlay.classList.add('hidden');
 }
 
+function closeModalOnEsc(e) {
+  if (e.keyCode === ESC_KEY_CODE && !modal.classList.contains('hidden'))
+    closeModal();
+}
+
 btnShow.forEach(btn => {
   btn.addEventListener('click', showModal);
 });
 
 btnClose.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', closeModalOnEsc);
